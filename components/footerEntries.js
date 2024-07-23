e16d1290456a2bafc99e2d13143992917a1f9ad7
avalle <ani.valle@bsc.es>
2024-07-23 15:28:08 +0200

    Create footer

---
+++ components/footerEntries.js
@@ -0,0 +1,24 @@
+// Displayed in Header and Footer
+
+const footerEntries = [
+	{
+		title: 'Benchmarking',
+		to: '/benchmarking',
+	},
+	{
+		title: 'Projects',
+		to: '/projects',
+	},
+	{
+		title: 'Tools',
+		to: '/tool',
+	},
+	{
+		title: 'Docs',
+		title_hide: 'Project Docs',
+		href: 'https://openebench.readthedocs.io/en/latest/introduction/1_overview.html',
+		external: true,
+	},
+];
+
+export default footerEntries;
