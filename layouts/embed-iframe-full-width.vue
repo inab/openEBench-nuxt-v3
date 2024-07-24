cb3b8e0c51a217861baa7499a842ec1e02a13ee3
avalle <ani.valle@bsc.es>
2024-07-24 16:30:00 +0200

    Update layout of embed-iframe

--- layouts/embed-iframe-full-width.vue
+++ layouts/embed-iframe-full-width.vue
@@ -1,14 +1,24 @@
 <template>
-	<div class="embed-iframe-full-width">
-		<HeaderMenu />
-    	<BreadcrumbsBar />
-	</div>
-	
+  <div>
+      <HeaderMenu />
+      <div class="py-5 my-3"></div>
+      <BreadcrumbsBar />
+      <NuxtPage />
+      <TheFooter />
+      <div>
+          <footer class="text-center my-1">
+              Made with <font-awesome-icon :icon="['fas', 'heart']" style="color: #dc2626;" />
+        in Barcelona.
+          </footer>
+      </div>
+  </div>
+
 </template>
 
 <script setup lang="ts">
 import HeaderMenu from "~/components/Header/HeaderMenu.vue"
 import BreadcrumbsBar from "~/components/Common/BreadcrumbsBar.vue"
+import TheFooter from "~/components/TheFooter.vue"
 
 useHead
 ({
