ed10b6981fe4eac97421716f6949af606944610c
Jessica F. Martinez <jessicafndez@gmail.com>
2024-07-24 17:41:55 +0200

    UPDATE challenge

---
+++ components/Common/CustomChip.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="custom-chip bg-zinc-100"
+        :class="selected?'bg-secondaryOeb-100 text-secondaryOeb-600':''">
+        <slot />
+    </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+    selected: boolean
+}>()
+
+</script>
+
+<style scoped lang="scss">
+    .custom-chip {
+        align-items: center;
+        cursor: default;
+        display: inline-flex;
+        line-height: 20px;
+        max-width: 100%;
+        outline: none;
+        overflow: hidden;
+        padding: 0 12px;
+        position: relative;
+        text-decoration: none;
+        transition-duration: .28s;
+        transition-property: box-shadow,opacity;
+        transition-timing-function: cubic-bezier(.4,0,.2,1);
+        vertical-align: middle;
+        white-space: nowrap;
+        border-radius: 16px;
+        font-size: 14px;
+        height: 32px;
+    }
+</style>
--- pages/benchmarking/community/challenge/index.vue
+++ pages/benchmarking/community/challenge/index.vue
@@ -1,108 +1,95 @@
 <template>
-    <div>This is challenges</div>
+    <div class="benchmarking-challenge container">
+        <div class="benchmarking-challenge__title">
+            {{ challenge.acronym }} ( {{ challenge._id }})
+        </div>
+        <div class="benchmarking-challenge__subtitle">
+            {{ challenge.name }}
+        </div>
+        <div class="benchmarking-challenge__body">
+            <div class="benchmarking-challenge__body_dec" v-for="(item, index) in datasets" :key="index">
+                <div v-if="index == tab && item">
+                    <ChartDescriptionCard
+                        :type="item.datalink.inline_data.visualization.type"
+                        :label="challenge.challenge_label"
+                    />
+                </div>
+            </div>
+            <div class="benchmarking-challenge__body__content">
+                <h2 class="text-h6 mt-8">
+                    Choose the metrics you want to visualize in the diagram:
+                </h2>
+                <div class="benchmarking-challenge__body__content__chips">
+                    <CustomChip v-for="(item, index) in datasets" :key="item._id"
+                        :selected="index == tab">
+                        <span v-if="item.datalink.inline_data.visualization.type == 'box-plot'"
+                        >
+                            {{ item.datalink.inline_data.visualization.available_metrics[0] }}
+                        </span>
+                        <span v-else>
+                            {{
+                                item.datalink.inline_data.visualization.type == '2D-plot'
+                                    ? item.datalink.inline_data.visualization.x_axis +
+                                    ' + ' +
+                                    item.datalink.inline_data.visualization.y_axis
+                                    : item.datalink.inline_data.visualization.metric
+                            }}
+                        </span>
+                    </CustomChip>
+                </div>
+                <div class="benchmarking-challenge__body__content__graphs">
+                    <div v-for="(item, index) in datasets" :key="item._id"
+                        class="benchmarking-challenge__body__content__graphs__items">
+                        <LoaderChartWidgets
+                            :data="item"
+                            :metrics="metrics"
+                        ></LoaderChartWidgets>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
-import LoaderChartWidgetsVue from "~/components/Widgets/LoaderChartWidgets";
+    import { ref } from 'vue'
+    import { challengeAPI } from "@/api/challengeAPI";
+    import  ChallengeObj  from "@/models/ChallengeObj";
+    import ChartDescriptionCard from '@/components/Cards/ChartDescriptionCard.vue';
+    import CustomChip from '@/components/Common/CustomChip.vue';
+    import LoaderChartWidgets from '@/components/Widgets/LoaderChartWidgets.vue';
 
-const { $graphql } = useNuxtApp()
-const route = useRoute()
-const runtimeConfig = useRuntimeConfig()
-const hostname = runtimeConfig.public.OEB_LEGACY_ANGULAR_URI
-const challengeId = route.params.challenge
-const datasets = ref(null)
+    const route = useRoute()
+    const challengeId = route.params.challenge
 
-const { data, pending }: { data: any, pending: boolean } = await useAsyncData('communities', () => $graphql('/graphql',
-{
-    method: 'POST',
-    headers: {
-        "Accept": "text/plain, */*",
-    },
-    body: JSON.stringify(
-        {
-            query: `
-                query ($id: String!) {
-                    getChallenges(challengeFilters: { id: $id }) {
-                        _id
-                        name
-                        acronym
-                        metrics_categories {
-                            metrics {
-                                metrics_id
-                                tool_id
-                            }
-                        }
-                        participant_datasets: datasets(datasetFilters: {type: "participant"}) {
-                            _id
-                            orig_id
-                            datalink {
-                                inline_data
-                                schema_url
-                                uri
-                                schema_uri
-                            }
+    let datasets = ref(Array)
+    let participants: any = ref(null)
+    let metrics = ref(null)
+    let challenge: any = ref(null)
 
-                            depends_on {
-                                tool_id
-                                metrics_id
-                                rel_dataset_ids {
-                                    dataset_id
-                                }
-                            }
-                            _metadata
-                        }
-                        assessment_datasets: datasets(datasetFilters: {type: "assessment"}) {
-                            _id
-                            orig_id
-                            datalink {
-                                inline_data
-                                schema_url
-                                uri
-                                schema_uri
-                            }
-                            dates {
-                                modification
-                            }
-                            depends_on {
-                                tool_id
-                                metrics_id
-                                rel_dataset_ids {
-                                    dataset_id
-                                }
-                            }
-                            _metadata
-                        }
-                    }
+    let responseData = await challengeAPI(challengeId)
 
-                    getDatasets(
-                        datasetFilters: { challenge_id: $id, type: "aggregation" }
-                    ) {
-                        datalink {
-                            inline_data
-                        }
-                        dates {
-                            modification
-                        }
-                        _id
-                    }
+    let challengeObj = new ChallengeObj(challengeId, responseData.data)
+    challengeObj.formatData()
+    challenge.value = challengeObj.getChallenge()
+    participants.value = challengeObj.getParticipants()
+    metrics.value = challengeObj.getMetrics()
+    datasets.value = challengeObj.getDatasets()
 
-                    getMetrics {
-                        _id
-                        title
-                        orig_id
-                        representation_hints
-                        _metadata
-                    }
-                }
-            `,
-            variables: {
-                id: challengeId,
-            },
-        }
-    )
-}
-))
+    console.log(challenge.value)
 
-datasets.value = data.value.data.getDatasets ?? null
+    let tab = ref(0)
+
 </script>
+
+<style scoped lang="scss">
+    .benchmarking-challenge__body__content__chips {
+        white-space: normal;
+        flex-wrap: wrap;
+        max-width: 100%;
+        display: flex;
+        transition: .3s cubic-bezier(.25,.8,.5,1);
+        flex: 1 0 auto;
+        gap: 10px;
+    }
+</style>
