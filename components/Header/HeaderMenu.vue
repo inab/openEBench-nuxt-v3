084fe8d4d7958f0cb6fcd34142a03d7a33dedd84
avalle <ani.valle@bsc.es>
2024-08-02 14:01:23 +0200

    Add new responsive navbar

--- components/Header/HeaderMenu.vue
+++ components/Header/HeaderMenu.vue
@@ -8,7 +8,7 @@
                             <img src="/assets/images/opeb_logo.gif" alt="OpenEBench" width="80" class="navbar-logo d-inline-block align-top" />
                         </NuxtLink>
                     </div>
-                    <nav>
+                    <nav class="flex-grow">
                         <div class="nav-mobile">
                             <button class="navbar-burger flex items-center text-blue-600 p-3" id="navbar-toggle"
                             :class="[toggleMenu?'active':'collapsing']"
@@ -16,8 +16,7 @@
                                 <span></span>
                             </button>
                         </div>
-                        <div class="nav-list w-100" 
-                        v-if="((toggleMenu && isMobile) || !isMobile)">
+                        <div :class="{'nav-offcanvas': true, 'show': toggleMenu}">
                             <div class="nav-list-items top-full">
                                 <ul class="nav-list-items-ul w-100">
                                     <li>
@@ -57,7 +56,7 @@
                                         </ul>
                                     </li>
                                     <li class="relative parent">
-                                        <div href="#" class="nav-link flex justify-between md:inline-flex items-center hover:bg-gray-50 space-x-2">
+                                        <div class="nav-link flex justify-between md:inline-flex items-center hover:bg-gray-50 space-x-2">
                                             <span>About</span>
                                             <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 fill-current pt-1" viewBox="0 0 24 24"><path d="M0 7.33l2.829-2.83 9.175 9.339 9.167-9.339 2.829 2.83-11.996 12.17z"/></svg>
                                         </div>
@@ -77,17 +76,16 @@
                                 </ul>
                             </div>
                             <div class="nav-list-items-direct top-full hidden sm:flex">
-                                <button @click="handleLogin" 
-                                class="text-gray-800 text-sm font-semibold hover:text-purple-600 mr-4"
-                                >
+                                <button @click="handleLogin" class="text-gray-800 text-sm font-semibold hover:text-purple-600 mr-4">
                                     Login
                                 </button>
-                                <a :href="runtimeConfig.public.VRE_URI" target="_blank" class="text-gray-800 text-sm font-semibold border px-4 py-2 rounded-lg hover:text-purple-600 hover:border-purple-600"
-                                @click="closeMenu">
+                                <a :href="runtimeConfig.public.VRE_URI" target="_blank" class="text-gray-800 text-sm font-semibold border px-4 py-2 rounded-lg hover:text-purple-600 hover:border-purple-600" @click="closeMenu">
                                     Benchmark your tool
                                 </a>
                             </div>
                         </div>
+                        <!-- Overlay div -->
+                        <div v-if="toggleMenu" class="nav-overlay" @click="handleToggleMenu"></div>
                     </nav>
                 </div>
             </div>
@@ -143,55 +141,36 @@ function closeMenu() {
 
 
 <style scoped lang="scss">
-    .navbar {
-        height: 65px; 
-        font-size: 18px;
-        border-bottom: 1px solid rgb(229, 231, 235);
-        position: fixed;
-        top: 0;
-        z-index: 2;
-        .navbar-brand {
-            height: 65px;
-            img {
-                width: 85px;
-            }
-        }
-        .nav-link {
-            font-size: 18px;
-            font-weight: 500;
-            padding: 22px 10px 15px;
-            display: flex;
-            color: rgba(0, 0, 0, 0.65);
-            cursor: pointer;
-            &:hover {
-                color: #0b579f;
-            }
-        }
-        #navbarSupportedContent {
-            height: 100%;
-        }
-        .form-inline {
-            display: flex;
-            gap: 30px;
-            align-items: center;
-            a {
-                color: #0b579f;
-                &.btn:hover {
-                    color: #0b579f;
-                }
-                &.btn-link:hover {
-                    color: white;
-                    background-color: #0b579f;
-                }
-            }
-        }
-        a {
-            text-decoration: none;
-            color: rgba(0, 0, 0, .65);
-            font-size: 1rem;
-            font-weight: 500;
+.navbar {
+    height: 65px;
+    font-size: 18px;
+    border-bottom: 1px solid rgb(229, 231, 235);
+    position: fixed;
+    top: 0;
+    z-index: 2;
+    width: 100%;
+    background-color: white;
+
+    .navbar-brand {
+        height: 65px;
+        img {
+            width: 85px;
         }
     }
+
+    .nav-link {
+        font-size: 18px;
+        font-weight: 500;
+        padding: 22px 10px 15px;
+        display: flex;
+        color: rgba(0, 0, 0, 0.65);
+        cursor: pointer;
+
+        &:hover {
+            color: #0b579f;
+        }
+    }
+
     .nav-mobile {
         display: none;
         position: absolute;
@@ -208,12 +187,12 @@ function closeMenu() {
         justify-content: space-between;
         gap: 10px;
     }
-    
+
     .header-links li span {
         position: relative;
         z-index: 0;
     }
-    
+
     .header-links li span::before {
         content: '';
         position: absolute;
@@ -224,7 +203,7 @@ function closeMenu() {
         display: block;
         height: 6px;
     }
-    
+
     .header-links li.active span::before {
         background-color: #fcae04;
     }
@@ -234,17 +213,21 @@ function closeMenu() {
     }
 
     .child {
-        opacity:0;
-        height:0;
-        overflow:hidden;
+        opacity: 0;
+        height: 0;
+        overflow: hidden;
         transform: translateY(-10%);
         padding-left: 0px;
+
         ul {
             padding-left: 15px;
+
             li {
                 padding-bottom: 15px;
+
                 &:hover {
                     color: theme('colors.primary.500');
+
                     a {
                         color: theme('colors.primary.500');
                     }
@@ -254,9 +237,9 @@ function closeMenu() {
     }
 
     .parent:hover .child {
-        opacity:1;
-        height:auto;
-        overflow:auto;
+        opacity: 1;
+        height: auto;
+        overflow: auto;
         transform: translateY(0);
     }
 
@@ -266,161 +249,290 @@ function closeMenu() {
         top: 15px;
         cursor: pointer;
         padding: 10px 35px 16px 0px;
-   }
-    #navbar-toggle span, #navbar-toggle span:before, #navbar-toggle span:after {
+    }
+
+    #navbar-toggle span,
+    #navbar-toggle span:before,
+    #navbar-toggle span:after {
         cursor: pointer;
         border-radius: 1px;
         height: 3px;
         width: 30px;
-        background: theme('colors.primary.500');;
+        background: theme('colors.primary.500');
         position: absolute;
         display: block;
         content: '';
         transition: all 300ms ease-in-out;
     }
+
     #navbar-toggle span:before {
         top: -10px;
     }
+
     #navbar-toggle span:after {
         bottom: -10px;
     }
+
     #navbar-toggle.active span {
         background-color: transparent;
     }
-    #navbar-toggle.active span:before, #navbar-toggle.active span:after {
+
+    #navbar-toggle.active span:before,
+    #navbar-toggle.active span:after {
         top: 0;
     }
+
     #navbar-toggle.active span:before {
         transform: rotate(45deg);
     }
+
     #navbar-toggle.active span:after {
         transform: rotate(-45deg);
     }
+
+    .nav-offcanvas {
+        position: fixed;
+        top: 0;
+        left: -300px;
+        width: 300px;
+        height: 100%;
+        background-color: white;
+        transition: left 0.3s ease-in-out;
+        z-index: 2;
+    }
+
+    .nav-offcanvas.show {
+        left: 0;
+    }
+
     nav {
         flex-grow: 1;
         display: flex;
         justify-content: center;
         align-items: center;
     }
+
     .nav-list {
         display: flex;
         align-items: center;
         justify-content: center;
     }
+
     .nav-list-items {
         display: flex;
         flex-grow: 1;
+        flex-direction: column;
     }
+
     .nav-list-items-ul {
         display: flex;
+        flex-direction: column;
         justify-content: center;
         list-style: none;
         padding: 0;
         margin: 0;
     }
+
     .nav-list-items-direct {
-        display: flex;
+        display: flex !important;
         align-items: center;
-        justify-content: end;
-    }
+        justify-content: end !important;
+        position: absolute;
+        right: 20px;
+        top: 0px;
 
-    @media only screen and (min-width: 950px) {
-        .nav-list {
-            display: flex;
-        }
-        
-    }
-
-    @media only screen and (max-width: 1000px) {
-        .nav-wrapper  {
-            background-color: white;
-        }
-        .nav-list {
-            display: block;
-            background-color: white;
-        }
-        .navbar-brand {
-            position: absolute;
-            top: 0;
-            background: transparent;
-            height: 55px;
-            width: 70px;
-        }
-        .nav-mobile {
-            display: block;
-        }
-        .nav-list-items {
-            display: block;
-        }
-        .nav-list-items-ul {
-            display: block;
-        }
-        nav {
-            width: 100%;
-            padding: 55px 0 15px;
-            flex-grow:1;
-        }
-        nav ul {
-            display: none;
-        }
-        nav ul li {
-            float: none;
-        }
-        nav ul li a {
-            padding: 15px;
-            line-height: 20px;
-        }
-        nav ul li ul li a {
-            padding-left: 30px;
-        }
-        .navbar-dropdown {
+        @media only screen and (max-width: 950px) {
             position: static;
+            justify-content: start !important;
+            margin-top: 20px;
+            width: 100%;
+            padding: 10px;
+            flex-direction: column;
+            // border-top: 1px solid #e5e7eb;
         }
-        #navbar-toggle.active span::before {
-            transform: rotate(45deg);
-        }
-        #navbar-toggle.active span::after {
-            transform: rotate(-45deg);
-        }
-        #navbar-toggle {
-            position: absolute;
-            left: 0px;
-            top: 15px;
-            cursor: pointer;
-            padding: 10px 35px 16px 0px;
-            width: 45px;
-       }
-        #navbar-toggle span, #navbar-toggle span:before, #navbar-toggle span:after {
-            cursor: pointer;
-            border-radius: 1px;
-            height: 3px;
-            width: 90px;
-            background: #0b579f;
-            position: absolute;
-            display: block;
-            content: '';
-            transition: all 300ms ease-in-out;
-       }
-        #navbar-toggle span:before {
-            top: -10px;
-       }
-        #navbar-toggle span:after {
-            bottom: -10px;
-       }
-        #navbar-toggle.active span {
-            background-color: transparent;
-       }
-        #navbar-toggle.active span:before, #navbar-toggle.active span:after {
-            top: 0;
-       }
-        #navbar-toggle.active span:before {
-            transform: rotate(45deg);
-            width: 30px;
-       }
-        #navbar-toggle.active span:after {
-            transform: rotate(-45deg);
-            width: 30px;
-       }
-        
     }
+
+.nav-offcanvas {
+    position: fixed;
+    top: 0;
+    left: -300px;
+    width: 300px;
+    height: 100%;
+    background-color: white;
+    transition: left 0.3s ease-in-out;
+    z-index: 2;
+}
+
+.nav-offcanvas.show {
+    left: 0;
+}
+
+.nav-overlay {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 0;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.4);
+    z-index: 1;
+    transition: width 0.3s ease-in-out, opacity 0.3s ease-in-out;
+    opacity: 0;
+}
+
+.nav-offcanvas.show + .nav-overlay {
+    transition: width 0.3s ease-in-out, opacity 0.3s ease-in-out;
+    width: 100%;
+    opacity: 1;
+    display: block;
+}
+
+.nav-overlay {
+    display: none;
+}
+
+@media only screen and (min-width: 950px) {
+    .nav-list {
+        display: flex;
+    }
+
+    .nav-mobile {
+        display: none;
+    }
+
+    .nav-offcanvas {
+        position: static;
+        width: auto;
+        height: auto;
+        display: flex;
+        flex-direction: row;
+        background-color: transparent;
+        transform: none;
+        transition: none;
+    }
+
+    .nav-list-items {
+        flex-direction: row;
+    }
+
+    .nav-list-items-ul {
+        flex-direction: row;
+    }
+}
+
+@media only screen and (max-width: 950px) {
+    .nav-wrapper {
+        background-color: white;
+    }
+
+    .nav-list {
+        display: block;
+        background-color: white;
+    }
+
+    .navbar-brand {
+        position: absolute;
+        top: 0;
+        background: transparent;
+        height: 55px;
+        width: 70px;
+    }
+
+    .nav-mobile {
+        display: block;
+    }
+
+    .nav-list-items {
+        display: block;
+    }
+
+    .nav-list-items-ul {
+        display: block;
+    }
+
+    nav {
+        width: 100%;
+        padding: 55px 0 15px;
+        flex-grow: 1;
+    }
+
+    nav ul {
+        display: none;
+    }
+
+    nav ul li {
+        float: none;
+    }
+
+    nav ul li a {
+        padding: 15px;
+        line-height: 20px;
+    }
+
+    nav ul li ul li a {
+        padding-left: 30px;
+    }
+
+    .navbar-dropdown {
+        position: static;
+    }
+
+    #navbar-toggle.active span::before {
+        transform: rotate(45deg);
+    }
+
+    #navbar-toggle.active span::after {
+        transform: rotate(-45deg);
+    }
+
+    #navbar-toggle {
+        position: absolute;
+        left: 0px;
+        top: 15px;
+        cursor: pointer;
+        padding: 10px 35px 16px 0px;
+        width: 45px;
+    }
+
+    #navbar-toggle span,
+    #navbar-toggle span:before,
+    #navbar-toggle span:after {
+        cursor: pointer;
+        border-radius: 1px;
+        height: 3px;
+        width: 30px;
+        background: #0b579f;
+        position: absolute;
+        display: block;
+        content: '';
+        transition: all 300ms ease-in-out;
+    }
+
+    #navbar-toggle span:before {
+        top: -10px;
+    }
+
+    #navbar-toggle span:after {
+        bottom: -10px;
+    }
+
+    #navbar-toggle.active span {
+        background-color: transparent;
+    }
+
+    #navbar-toggle.active span:before,
+    #navbar-toggle.active span:after {
+        top: 0;
+    }
+
+    #navbar-toggle.active span:before {
+        transform: rotate(45deg);
+        width: 30px;
+    }
+
+    #navbar-toggle.active span:after {
+        transform: rotate(-45deg);
+        width: 30px;
+    }
+}
+}
 </style>
