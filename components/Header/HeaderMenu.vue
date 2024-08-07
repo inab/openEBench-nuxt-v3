b3e04f43483734a074c3cb7e976335ac7608fb7d
avalle <ani.valle@bsc.es>
2024-08-07 16:23:46 +0200

    Fix #32

--- components/Header/HeaderMenu.vue
+++ components/Header/HeaderMenu.vue
@@ -37,39 +37,32 @@
                                             <span>Tools</span>
                                         </NuxtLink>
                                     </li>
-                                    <li class="relative parent">
-                                        <div class="nav-link flex justify-between md:inline-flex items-center hover:bg-gray-50 space-x-2">
+                                    <!-- Dropdown -->
+                                    <li class="nav-item dropdown">
+                                        <a class="nav-link dropdown-toggle flex justify-between md:inline-flex items-center hover:bg-gray-50 space-x-2" href="#" id="observatoryDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                             <span>Observatory</span>
                                             <font-awesome-icon :icon="['fas', 'chevron-down']" size="sm" />
-                                        </div>
-                                        <ul class="child transition duration-300 md:absolute top-full left-0 md:w-40 bg-white md:shadow-lg md:rounded-b">
-                                            <li>
-                                                <ul class="my-2">
-                                                    <li v-for="(item, index) in subMenuEntriesObservatory" class="hover:bg-gray-100 ps-3 text-sm" @click="closeMenu"
-                                                        :key="index">
-                                                        <NuxtLink :to="item.to">
-                                                            {{ item.title }}
-                                                        </NuxtLink>
-                                                    </li>
-                                                </ul>
+                                        </a>
+                                        <ul class="dropdown-menu shadow-xl" aria-labelledby="observatoryDropdown">
+                                            <li v-for="(item, index) in subMenuEntriesObservatory" :key="index" @click="closeMenu" 
+                                            class="hover:bg-gray-100 text-sm py-2">
+                                                <NuxtLink :to="item.to">
+                                                {{ item.title }}
+                                                </NuxtLink>
                                             </li>
                                         </ul>
                                     </li>
-                                    <li class="relative parent">
-                                        <div class="nav-link flex justify-between md:inline-flex items-center hover:bg-gray-50 space-x-3">
+                                    <li class="nav-item dropdown">
+                                        <a class="nav-link dropdown-toggle flex justify-between md:inline-flex items-center hover:bg-gray-50 space-x-2" href="#" id="aboutDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                             <span>About</span>
-                                            <font-awesome-icon :icon="['fas', 'chevron-down']" size="sm"/>
-                                        </div>
-                                        <ul class="child transition duration-300 md:absolute top-full left-0 md:w-28 bg-white md:shadow-lg md:rounded-b">
-                                            <li>
-                                                <ul class="my-2">
-                                                    <li v-for="(item, index) in subMenuEntriesAbout" class="hover:bg-gray-100 ps-3 text-sm " @click="closeMenu"
-                                                        :key="index">
-                                                        <NuxtLink :to="item.to">
-                                                            {{ item.title }}
-                                                        </NuxtLink>
-                                                    </li>
-                                                </ul>
+                                            <font-awesome-icon :icon="['fas', 'chevron-down']" size="sm" />
+                                        </a>
+                                        <ul class="dropdown-menu submenu-about shadow-xl md:rounded-b" aria-labelledby="aboutDropdown">
+                                            <li v-for="(item, index) in subMenuEntriesAbout" :key="index" @click="closeMenu" 
+                                            class="hover:bg-gray-100 text-sm py-2">
+                                                <NuxtLink :to="item.to">
+                                                {{ item.title }}
+                                                </NuxtLink>
                                             </li>
                                         </ul>
                                     </li>
@@ -172,7 +165,6 @@ function closeMenu() {
         display: flex;
         color: rgba(0, 0, 0, 0.65);
         cursor: pointer;
-
         &:hover {
             color: #0b579f;
         }
@@ -187,131 +179,7 @@ function closeMenu() {
         height: 55px;
         width: 70px;
     }
-
-    .header-menu-right {
-        display: flex;
-        flex-flow: row wrap;
-        justify-content: space-between;
-        gap: 10px;
-    }
-
-    .header-links li span {
-        position: relative;
-        z-index: 0;
-    }
-
-    .header-links li span::before {
-        content: '';
-        position: absolute;
-        z-index: -1;
-        bottom: 2px;
-        left: -4px;
-        right: -4px;
-        display: block;
-        height: 6px;
-    }
-
-    .header-links li.active span::before {
-        background-color: #fcae04;
-    }
-
-.header-links li:not(.active):hover span::before {
-  background-color: #ccc;
-}
-
-    .child {
-        opacity: 0;
-        height: 0;
-        overflow: hidden;
-        transform: translateY(-10%);
-        padding-left: 0px;
-
-        ul {
-            padding-left: 0px;
-            li {
-                padding-top: 10px;
-                padding-bottom: 10px;
-                a {
-                    text-decoration: none !important;
-                    color: rgb(0, 0, 0);
-                }
-            }
-        }
-    }
-
-    .optionsHeader{
-        text-decoration: none !important;
-    }
-
-    .parent:hover .child {
-        opacity: 1;
-        height: auto;
-        overflow: auto;
-        transform: translateY(0);
-    }
-
-    #navbar-toggle {
-        position: absolute;
-        left: 18px;
-        top: 15px;
-        cursor: pointer;
-        padding: 10px 35px 16px 0px;
-    }
-
-    #navbar-toggle span,
-    #navbar-toggle span:before,
-    #navbar-toggle span:after {
-        cursor: pointer;
-        border-radius: 1px;
-        height: 3px;
-        width: 30px;
-        background: theme('colors.primary.500');
-        position: absolute;
-        display: block;
-        content: '';
-        transition: all 300ms ease-in-out;
-    }
-
-    #navbar-toggle span:before {
-        top: -10px;
-    }
-
-    #navbar-toggle span:after {
-        bottom: -10px;
-    }
-
-    #navbar-toggle.active span {
-        background-color: transparent;
-    }
-
-    #navbar-toggle.active span:before,
-    #navbar-toggle.active span:after {
-        top: 0;
-    }
-
-    #navbar-toggle.active span:before {
-        transform: rotate(45deg);
-    }
-
-    #navbar-toggle.active span:after {
-        transform: rotate(-45deg);
-    }
-
-    .nav-offcanvas {
-        position: fixed;
-        top: 0;
-        left: -300px;
-        width: 300px;
-        height: 100%;
-        background-color: white;
-        transition: left 0.3s ease-in-out;
-        z-index: 2;
-    }
-
-    .nav-offcanvas.show {
-        left: 0;
-    }
-
+  
     nav {
         flex-grow: 1;
         display: flex;
@@ -358,48 +226,65 @@ function closeMenu() {
             padding: 10px;
             flex-direction: column;
             gap: 10px;
-            // border-top: 1px solid #e5e7eb;
         }
     }
 
-.nav-offcanvas {
-    position: fixed;
-    top: 0;
-    left: -300px;
-    width: 300px;
-    height: 100%;
-    background-color: white;
-    transition: left 0.3s ease-in-out;
-    z-index: 2;
-}
+    .nav-offcanvas {
+        position: fixed;
+        top: 0;
+        left: -300px;
+        width: 300px;
+        height: 100%;
+        background-color: white;
+        transition: left 0.3s ease-in-out;
+        z-index: 2;
+    }
 
-.nav-offcanvas.show {
-    left: 0;
-}
+    .nav-offcanvas.show {
+        left: 0;
+    }
 
-.nav-overlay {
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 0;
-    height: 100%;
-    background-color: rgba(0, 0, 0, 0.4);
-    z-index: 1;
-    transition: width 0.3s ease-in-out, opacity 0.3s ease-in-out;
-    opacity: 0;
-}
+    .nav-overlay {
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 0;
+        height: 100%;
+        background-color: rgba(0, 0, 0, 0.4);
+        z-index: 1;
+        transition: width 0.3s ease-in-out, opacity 0.3s ease-in-out;
+        opacity: 0;
+    }
 
-.nav-offcanvas.show + .nav-overlay {
-    transition: width 0.3s ease-in-out, opacity 0.3s ease-in-out;
-    width: 100%;
-    opacity: 1;
-    display: block;
-}
+    .nav-offcanvas.show + .nav-overlay {
+        transition: width 0.3s ease-in-out, opacity 0.3s ease-in-out;
+        width: 100%;
+        opacity: 1;
+        display: block;
+    }
 
-.nav-overlay {
-    display: none;
-}
+    // Options
+    .dropdown-menu {
+        border: none;
+        margin-top: 0;
+        border-radius: 0px 0px 5px 5px;
+    }
 
+    .submenu-about {
+       min-width: 110px;
+    }
+
+    .dropdown-toggle::after {
+        display: none;
+    }
+
+    nav ul li ul li a {
+        padding-left: 10px;
+        text-decoration: none !important;
+        color: rgba(0, 0, 0, 0.87);
+    }
+
+// ipad
 @media only screen and (min-width: 950px) {
     .nav-list {
         display: flex;
@@ -429,6 +314,7 @@ function closeMenu() {
     }
 }
 
+// Movil
 @media only screen and (max-width: 950px) {
     .nav-wrapper {
         background-color: white;
@@ -451,26 +337,12 @@ function closeMenu() {
         display: block;
     }
 
-    .nav-list-items {
-        display: block;
-    }
-
-    .nav-list-items-ul {
-        display: block;
-    }
-
-    nav {
-        width: 100%;
-        padding: 55px 0 15px;
-        flex-grow: 1;
-    }
-
-    nav ul {
-        display: block;
-    }
-
-    nav ul li {
-        float: none;
+    // Options
+    .dropdown-menu {
+        position: relative;
+        border-radius: 0;
+        box-shadow: none;
+        background-color:#F9FAFB
     }
 
     nav ul li a {
@@ -479,21 +351,11 @@ function closeMenu() {
     }
 
     nav ul li ul li a {
-        padding-left: 30px;
+        padding-left: 50px;
+        text-decoration: none !important;
     }
 
-    .navbar-dropdown {
-        position: static;
-    }
-
-    #navbar-toggle.active span::before {
-        transform: rotate(45deg);
-    }
-
-    #navbar-toggle.active span::after {
-        transform: rotate(-45deg);
-    }
-
+    // Button navbar-dropdown
     #navbar-toggle {
         position: absolute;
         left: 0px;
