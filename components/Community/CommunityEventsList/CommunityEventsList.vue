d3b8eabcb0e0dfb4c711ed6ac7e738529b5df2ab
avalle <ani.valle@bsc.es>
2024-08-12 16:43:15 +0200

    Responsive Events page

--- components/Community/CommunityEventsList/CommunityEventsList.vue
+++ components/Community/CommunityEventsList/CommunityEventsList.vue
@@ -8,13 +8,13 @@
         <h3>Please choose an event:</h3>
         <div class="community-events-list__header__subtitle">
           <p
-            class="text-body-2 text--secondary d-flex align-center text-stone-500"
+            class="text-body-2 text--secondary d-flex align-items-center text-stone-500"
           >
             <span class="mt-1 me-3">
               <UIcon name="i-heroicons-information-circle" scale="2"></UIcon>
             </span>
             An Event corresponds to a time-bound contest where a tool, pipeline,
-            service or product, i.e. the participant, is compared against other
+            service, or product, i.e. the participant, is compared against other
             participants using a predefined collection of reference datasets and
             assessment metrics.
           </p>
@@ -24,7 +24,7 @@
         <EventCard
           v-for="event in props.events"
           :key="event._id"
-          class="col-4"
+          class="col-12 col-sm-6 col-md-4 mb-4"
           :event="event"
           :community-id="communityId"
         />
