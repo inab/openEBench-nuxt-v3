4b2b743964f7974d1440e5c1e107853277f9b096
Andrea <andrea.morales@bsc.es>
2024-09-10 09:52:10 +0200

    Added the breadcrumbs, does not re-route well, remain fix

--- pages/benchmarking/community/challenge/index.vue
+++ pages/benchmarking/community/challenge/index.vue
@@ -1,4 +1,5 @@
 <template>
+  <BreadcrumbsBar :breadcrumbs-array="routeArray" />
   <div class="benchmarking-challenge container">
     <div class="benchmarking-challenge__title text-primaryOeb-500">
       {{ challenge.acronym }} ( {{ challenge._id }})
@@ -7,16 +8,11 @@
       {{ challenge.name }}
     </div>
     <div class="benchmarking-challenge__body">
-      <div
-        v-for="(item, index) in datasets"
-        :key="index"
-        class="benchmarking-challenge__body_dec text-gray-500 text-sm"
-      >
+      <div v-for="(item, index) in datasets" :key="index"
+        class="benchmarking-challenge__body_dec text-gray-500 text-sm">
         <div v-if="index == tab && item">
-          <ChartDescriptionCard
-            :type="item.datalink.inline_data.visualization.type"
-            :label="challenge.challenge_label"
-          />
+          <ChartDescriptionCard :type="item.datalink.inline_data.visualization.type"
+            :label="challenge.challenge_label" />
         </div>
       </div>
       <div class="benchmarking-challenge__body__content text-sm">
@@ -37,18 +33,26 @@ import { challengeAPI, getGraphData } from "@/api/challengeAPI";
 import ChallengeObj from "@/models/ChallengeObj";
 import ChartDescriptionCard from "@/components/Cards/ChartDescriptionCard.vue";
 import CustomTabs from "@/components/Widgets/CustomTabs.vue";
+import BreadcrumbsBar from "@/components/Common/BreadcrumbsBar.vue";
 
 const route = useRoute();
+const isPending = ref(false);
+const communityStore = useCommunity();
+const communityId: string = route.params.community;
+const eventsObj: any[] = communityStore.getEvents;
+const community: Ref<any> = ref(null);
 const challengeId = route.params.challenge;
 
 const datasets = ref(Array);
-const participants = ref(Array);
+const participants: any = ref(null);
 const metrics = ref(null);
 const challenge: any = ref(null);
 const tab = ref(0);
 
 const itemSelected = ref(null);
 const items = ref([]);
+
+
 
 await challengeAPI(challengeId).then(async (response: any) => {
   const challengeObj = new ChallengeObj(challengeId, response.data);
@@ -57,6 +61,7 @@ await challengeAPI(challengeId).then(async (response: any) => {
   metrics.value = challengeObj.getMetrics();
   datasets.value = challengeObj.getDatasets();
   participants.value = challengeObj.getParticipants();
+  participants.value = Object.values(participants.value);
 
   for (const [_key, value] of Object.entries(datasets.value)) {
     const graphData = await getGraphData(value).then((response: any) => {
@@ -70,8 +75,8 @@ await challengeAPI(challengeId).then(async (response: any) => {
         ? (value as any).datalink.inline_data.visualization.available_metrics[0]
         : (value as any).datalink.inline_data.visualization.type == "2D-plot"
           ? (value as any).datalink.inline_data.visualization.x_axis +
-            " + " +
-            (value as any).datalink.inline_data.visualization.y_axis
+          " + " +
+          (value as any).datalink.inline_data.visualization.y_axis
           : (value as any).datalink.inline_data.visualization.metric;
 
     const newData = value;
@@ -86,6 +91,54 @@ await challengeAPI(challengeId).then(async (response: any) => {
 
   itemSelected.value = items.value[0];
 });
+
+if (communityStore.communityId === communityId) {
+  community.value = communityStore.getCommunityData;
+} else {
+  const { data, pending }: { data: any; pending: Ref<boolean> } = await useAsyncData(
+    "community",
+    () => communityStore.requestCommunityData(communityId, event),
+  );
+  community.value = data.value ?? null;
+  isPending.value = pending.value;
+}
+
+const currentEvent = computed(() => {
+  const selectedEvent = communityStore.getCurrentEvent;
+
+
+  // If no event is selected, select the first available event.
+  if (!selectedEvent && eventsObj.length > 0) {
+    const firstEvent = eventsObj[0];
+    communityStore.setCurrentEvent(firstEvent);
+    return firstEvent;
+  }
+  return selectedEvent;
+});
+
+const routeArray: Array<{ label: string; isActualRoute: boolean; route?: string }> = [
+  {
+    label: "Benchmarking Communities",
+    isActualRoute: false,
+    route: "/benchmarking",
+  },
+  {
+    label: community.value?.acronym + " " + "Events",
+    isActualRoute: false,
+    route: "/benchmarking/" + communityId + "/events",
+  },
+  {
+    label: currentEvent.value?.name,
+    isActualRoute: false,
+    route: "/benchmarking/" + communityId + "?event=" + currentEvent.value._id
+  },
+  {
+    label: "Challenge " + challenge.value.acronym + ' ' + challengeId,
+    isActualRoute: true,
+  },
+
+];
+
 </script>
 
 <style scoped lang="scss">
@@ -97,14 +150,17 @@ await challengeAPI(challengeId).then(async (response: any) => {
     line-height: 2.5rem;
     letter-spacing: 0.0073529412em !important;
   }
+
   &__subtitle {
     font-size: 18px;
     font-weight: 400;
     padding-bottom: 5px;
     color: black;
   }
+
   &__body {
     padding-top: 30px;
+
     &__content {
       &__title {
         font-size: 18px;
@@ -114,6 +170,7 @@ await challengeAPI(challengeId).then(async (response: any) => {
       }
     }
   }
+
   .benchmarking-challenge__body__content__chips {
     white-space: normal;
     flex-wrap: wrap;
