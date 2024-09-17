1b23242cb085564774c09e12c3bcf2b3701e966e
Andrea <andrea.morales@bsc.es>
2024-09-17 10:16:20 +0200

    Upgrade the layout of noData Available

--- layouts/noDataAvailable.vue
+++ layouts/noDataAvailable.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="no-data-available">
-    <NuxtImg src="/images/illustrations/empty-state.svg" />
-    <h3 class="mt-10 text-h5 text-center">No {{ item }} available.</h3>
+    <NuxtImg src="/images/illustrations/empty-state.svg" class="image mt-5" />
+    <h4 class="mt-10 text text-center">No {{ item }} available.</h4>
   </div>
 </template>
 
@@ -10,3 +10,22 @@ defineProps<{
   item: any;
 }>();
 </script>
+
+<style lang="scss" scoped>
+.no-data-available {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  height: 100%;
+  .image {
+    width: 450px; 
+    height: auto;
+  }
+  .text{
+   font-size: 24px;
+   font-weight: 400;
+  }
+}
+</style>
