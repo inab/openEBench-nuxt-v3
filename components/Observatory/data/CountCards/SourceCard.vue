d62318507c547824e426a566ab67299f70ff5eff
avalle <ani.valle@bsc.es>
2024-10-22 16:29:21 +0200

    Create content components of the MainCard

---
+++ components/Observatory/data/CountCards/SourceCard.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="card rounded pr-3 pt-2 pl-3 pb-1">
+    <div class="items-center">
+      <div class="grid-cols-4">
+        <UAvatar
+          :src="getImagePath(label)"
+          alt="Avatar"
+          class="avatar"
+        />
+      </div>
+      <div id="counts-text" class="grid-cols-8">
+        <span><h3> {{ title }}</h3></span>
+        <span><h2> {{ formatNumber(Number(count)) }}</h2></span>
+      </div>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+
+// PROPS
+const props = defineProps<{
+  title: string,
+  count: number,
+  label: string,
+}>();
+
+function formatNumber(value: number): string {
+  return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
+
+// Función para obtener la imagen correcta desde el path
+function getImagePath(label: string): string {
+  return new URL(`/images/observatory/${label}-logo.svg`, import.meta.url).href
+}
+</script>
+
+<style scoped>
+#counts-text {
+	text-align: right !important;
+}
+
+.avatar {
+	width: 85%;
+}
+
+.card {
+	min-height: 4.5rem;
+	width: 100%;
+	min-width: 8em;
+	display: inline-block;
+}
+
+.card h3 {
+	font-family: 'Open Sans', sans-serif;
+	font-size: 0.9rem;
+	font-weight: 300;
+	line-height: normal;
+	margin: 0.2rem;
+}
+
+.card h2 {
+	font-family: 'Open Sans', sans-serif;
+	font-size: 1rem;
+	font-weight: 500;
+	line-height: normal;
+	margin: 0.3rem;
+}
+</style>
---
+++ components/Observatory/data/CountCards/TotalCard.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="flex justify-around main-row rounded shadow-md py-2">
+    <div>
+      <p class="text-center number-text mt-1"> Total </p>
+      <p class="text-center number"> {{ formatNumber(Number(count)) }} </p>
+    </div>
+  </div>
+
+</template>
+
+<script setup lang="ts">
+
+// PROPS
+const props = defineProps<{
+  count: number,
+}>();
+
+console.log(props.count);
+
+function formatNumber(value:number) {
+  console.log(value);
+
+	return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
+</script>
+
+<style scoped>
+.main-row {
+  max-width: 80%;
+  width: 55%;
+  background-color: #001752f8;
+}
+
+.number {
+	font-size: 2.1rem;
+	color: white !important;
+  margin-bottom: 0px;
+}
+
+.number-text {
+	color: white !important;
+	font-family: Roboto, sans-serif;
+	font-size: 1.3em;
+	font-weight: 300;
+	line-height: 1em;
+	word-break: initial;
+  margin-bottom: 0px;
+}
+
+.number-highlight {
+	font-weight: 700;
+}
+</style>
