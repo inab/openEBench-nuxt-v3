fcff4453272fcd449c3f7dd714ed2e696981ddb0
avalle <ani.valle@bsc.es>
2024-10-25 14:54:05 +0200

    Add new frames for Plots

--- pages/observatory/Data.vue
+++ pages/observatory/Data.vue
@@ -43,21 +43,167 @@
           <MainCards/>
         </div>
 
+        <!-- Plots -->
+        <div class="row justify-center mt-4">
+
+          <!-- 1  -->
+          <div class="col-12">
+            <div class="my-4 p-4 max-full bg-white border border-gray-100 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+              <h5 class="text-center mt-6 mb-4">
+                Consolidated Research Software (meta)data
+              </h5>
+              <div class="row">
+                <div class="col-9">
+                  <!-- v-if -->
+                  <USkeleton v-if="store.unLoaded.features" class="h-52 mb-3 mx-10"/>
+                  <USkeleton v-if="store.unLoaded.features" class="h-7 w-80 rounded-xl mb-5 mx-10" />
+                  <!-- v-else -->
+                   <PlotOverview v-else />
+                </div>
+                <div class="col-3">
+                  <p class="mb-2">
+                    <span class="text-primaryOeb-600 text-base"
+                      >Features obtained from the different software metadata
+									    sources and total number of instances for which each feature
+									    exists in the dataset.</span>
+                  </p>
+                  <p class="mb-5">
+                    Dots (green: primary sources, red: secondary sources, blue:
+                    OpenEbench) show the availability of such features in the
+                    indicated sources. <br />
+                    Bars indicate the actual number of retrieved metadata items.
+                    Variation of the amounts is a consequence of the lack of
+                    completeness of metadata annotation.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <!-- 2 -->
+           <div class="col-12">
+            <div class="my-4 p-4 max-full bg-white border border-gray-100 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+              <h5 class="text-center mt-6 mb-4">
+                Instances Coverage
+              </h5>
+              <div class="row">
+                <div class="col-8">
+                  <!-- v-if -->
+                  <USkeleton v-if="store.unLoaded.features" class="h-52 mb-3 mx-10"/>
+                  <USkeleton v-if="store.unLoaded.features" class="h-7 w-80 rounded-xl mb-5 mx-10" />
+                  <!-- v-else -->
+                   <PlotSources v-else />
+                </div>
+                <div class="col-4">
+                  <p class="mb-2">
+                    <span class="text-primaryOeb-600 text-base"
+                      >Cumulative distribution of number of sources where individual
+                      instances are found (yellow).</span>
+                  </p>
+                  <p class="mb-5">
+                    Stacked bars represent the contribution of each source in terms
+                    of number of metadata entries. Sources used to discover software
+                    and build an initial collection of instances are coloured in
+                    shades of green, while those used to further enrich the initial
+                    collection are depicted in shades of red. OpenEBench, as an
+                    exception, is coloured in red while belonging to the latter
+                    category.
+                  </p>
+                </div>
+              </div>
+            </div>
+           </div>
+
+           <!-- 3 -->
+            <div class="col-6">
+              <div class="my-4 p-4 max-full bg-white border border-gray-100 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                <h5 class="text-center mt-6 mb-4">
+                  (Meta)data Completeness
+                </h5>
+                <div class="row">
+                  <div class="col-12">
+                    <!-- v-if -->
+                    <USkeleton v-if="store.unLoaded.completeness" class="h-52 mb-3 mx-10"/>
+                    <USkeleton v-if="store.unLoaded.completeness" class="h-7 w-80 rounded-xl mb-5 mx-10" />
+                    <!-- v-else -->
+                    <CompletenessPlot v-else />
+                  </div>
+                  <div class="col-12">
+                    <p class="mb-2">
+                      <span class="text-primaryOeb-600 text-base"
+                        >Distribution of number of features collected on individual
+                        instances.</span>
+                    </p>
+                    <p class="mb-5">
+                      The higher the number of features for an instance, the more
+                      information gathered about that instance.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+          <!-- 4 -->
+          <div class="col-6">
+            <div class="my-4 p-4 max-full bg-white border border-gray-100 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+              <h5 class="text-center mt-6 mb-4">
+						    Types Of Software
+					    </h5>
+              <div class="row">
+                <div class="col-12">
+                  <!-- v-if -->
+                  <USkeleton v-if="store.unLoaded.features" class="h-52 mb-3 mx-10"/>
+                  <USkeleton v-if="store.unLoaded.features" class="h-7 w-80 rounded-xl mb-5 mx-10" />
+                  <!-- v-else -->
+                   <PlotTypes v-else />
+                </div>
+                <div class="col-12">
+                  <p class="mb-2">
+                    <span class="text-primaryOeb-600 text-base"
+                      >Distribution of the types of instances in the
+                      collection.</span>
+                  </p>
+                  <p class="mb-5">
+                    Research Software may be implemented or accessed in different
+								    ways via web interface, REST API, command line, workflow, etc.
+								    Current types captured are (<i
+									  >cmd, web, db, app, lib, ontology, workflow, plugin, sparql,
+									    soap, script, rest, workbench, suite</i
+								    >)
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </NuxtLayout>
   </div>
 </template>
   
 <script setup lang="ts">
+import { useData } from '@/stores/observatory/data.js';
 import collectionSelector from "@/components/Observatory/CollectionSelector.vue"
 import MainCards from "@/components/Observatory/data/CountCards/MainCards.vue"
+import PlotOverview from "@/components/Observatory/data/PlotOverview.vue"
+import PlotSources from "@/components/Observatory/data/PlotSources.vue"
+import PlotTypes from "@/components/Observatory/data/PlotTypes.vue"
+import CompletenessPlot from "@/components/Observatory/data/CompletenessPlot.vue"
 
 const layout = 'observatory'
+
+const store = useData();
+
 
 </script>
 
 <style scoped>
 .illustration5{
   width: 80%;
+}
+
+p {
+  line-height: 22px;
+  font-size: 14px;
 }
 </style>
