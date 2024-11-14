2331c445aab26262a24c64c8e20524c3e4faec01
avalle <ani.valle@bsc.es>
2024-11-14 16:55:52 +0100

    Create PlotOptions

--- components/Observatory/PlotWOptions.vue
+++ components/Observatory/PlotWOptions.vue
@@ -1,6 +1,168 @@
 <template>
+  <div class="mb-0 pb-0 ml-auto mr-0 pr-0 pt-2">
+    <UTooltip text="See iframe code" 
+      :ui="{background:'bg-black bg-opacity-50', color:'text-white', 
+      transition:{
+        enterActiveClass: 'transition ease-out duration-300',
+        leaveActiveClass: 'transition ease-in duration-150',
+      }}"
+    >
+      <button 
+        @click="dialog = true" 
+        class="px-2 py-1 rounded-full hover:bg-gray-100 click-animation" 
+        aria-label="Copy icon"
+      >
+        <UIcon
+            name="i-oui-copy"
+            class="text-gray-500"
+        />
+      </button>
+    </UTooltip>
+    
+    <div v-if="dialog" class="fixed z-30 inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full flex items-center justify-center" @click.self="closeDialog">
+      <div :class="['relative mx-auto p-4 border w-11/12 md:w-2/3 shadow-lg rounded-md bg-white', dialogAnimation]">
+        <h5 class="mb-2">Plots Iframe Code</h5>
+        <p class="text-sm">Copy and paste the following code to embed the plots into your website. Change the width and height of the iframe as needed.</p>
+        <div class="space-y-4">
+          <div v-for="(item, index) in props.items" :key="index">
+            <h6 class="text-sm text-primaryOeb-600">{{ item.title }}</h6>
+            <div class="row">
+              <div class="col-11">
+                <pre class="bg-gray-100 p-2 rounded">
+                  {{ copyIframeCode(item.url).trim() }}
+                </pre>
+              </div>
+              <div class="col-1">
+              <UTooltip text="Copy to clipboard" :ui="{background:'bg-black bg-opacity-50', color:'text-white'}">
+                <button 
+                  @click="handleCopyClick(item.url)"
+                  class="px-2 py-1 rounded-full hover:bg-gray-100 click-animation"
+                >
+                <UIcon
+                    name="i-oui-copy"
+                    class="text-gray-500"
+                />
+                </button>
+              </UTooltip>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
 
+    <div v-if="copied" class="fixed z-40 bottom-10 left-1/2 rounded transform -translate-x-1/2 w-80 text-center p-2 bg-black bg-opacity-50 text-white flex justify-between items-center">
+      <span>Copied to clipboard</span>
+      <button @click="copied = false" class="ml-2 bg-transparent border-none text-red-600">Close</button>
+    </div>
+  </div>
 </template>
-<script setup lang="ts">
 
+<script setup lang="ts">
+import { ref, watch } from 'vue';
+
+const props = defineProps<{
+  items: Array<{ title: string, url: string }>;
+  currentCollection: string;
+}>();
+
+const dialog = ref(false);
+const copied = ref(false);
+const dialogAnimation = ref('an1');
+
+const copyIframeCode = (url: string) => {
+  const newUrl = `${url}/${props.currentCollection}/`;
+  return `<iframe src="${newUrl}"
+    frameborder="0"
+    style="width:50%; height:500px;"
+    scrolling="no">
+  </iframe>`;
+};
+
+// Show message Copied to clipboard
+const copyToClipboard = (code: string) => {
+  navigator.clipboard.writeText(code).then(() => {
+    showCopiedMessage();
+  }).catch(err => {
+    console.error('Failed to copy: ', err);
+  });
+};
+
+const showCopiedMessage = () => {
+  copied.value = true;
+  setTimeout(() => {
+    copied.value = false;
+  }, 30000); // Ocultar después de 30 segundos (30000 ms)
+};
+
+const handleCopyClick = (url: string) => {
+  const code = copyIframeCode(url);
+  copyToClipboard(code);
+};
+
+const closeDialog = () => {
+  dialogAnimation.value = 'an2';
+  setTimeout(() => {
+    dialog.value = false;
+    dialogAnimation.value = 'an1'; // Restablecer animación para la próxima apertura
+  }, 200); // Duración de la animación an2
+};
+
+watch(dialog, (newValue) => {
+  if (newValue) {
+    dialogAnimation.value = 'an1';
+  }
+});
 </script>
+
+<style scoped>
+.copy-icon {
+  cursor: pointer;
+}
+
+.hover\:bg-gray-300:hover {
+  background-color: #D1D5DB;
+}
+
+.click-animation {
+  transition: transform 0.1s ease-in-out, background-color 0.1s ease-in-out;
+}
+
+.click-animation:active {
+  transform: scale(0.98);
+  background-color: #c6d8e9;
+  border: 0.1px solid #084177;
+}
+
+.click-animation {
+  background-color: initial; 
+}
+
+.an1 {
+  animation: animationup 0.5s ease 0s 1 normal forwards;
+}
+
+@keyframes animationup {
+    0% {
+        transform: scale(0.5);
+    }
+
+    100% {
+        transform: scale(1);
+    }
+}
+
+.an2 {
+  animation: animationDown 0.5s ease 0s 1 normal forwards;
+}
+
+@keyframes animationDown {
+    0% {
+        transform: scale(1);
+    }
+
+    100% {
+        transform: scale(0);
+    }
+}
+</style>
