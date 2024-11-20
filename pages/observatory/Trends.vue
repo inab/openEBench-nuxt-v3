ed80cbb6aef317e5da4c8bef929f2668b4c87b47
Andrea <andrea.morales@bsc.es>
2024-11-20 19:43:57 +0100

    DOne with the first 2 plot (1 card)

--- pages/observatory/Trends.vue
+++ pages/observatory/Trends.vue
@@ -1,47 +1,44 @@
 <template>
   <div>
-  <NuxtLayout :name="layout">
-    <template #trends >
-      <div class="p-4">
-        <div class="row my-4">
-          <h5 class="">FAIRness Trends Analysis</h5>
-          <p style="line-height: 1.9" class="mb-2 mt-0">
-          Explore trends in key aspects to the FAIRness of research software.
-        </p>
-        </div>
-
-
-        <!-- collectionSelector -->
-        <div class="row mt-4">
-          <div class="col-2"></div>
-          <div class="col-8">
-            <collectionSelector />
+    <NuxtLayout :name="layout">
+      <template #trends>
+        <div class="p-4">
+          <div class="row my-4">
+            <h5 class="">FAIRness Trends Analysis</h5>
+            <p style="line-height: 1.9" class="mb-2 mt-0">
+              Explore trends in key aspects to the FAIRness of research software.
+            </p>
           </div>
-          <div class="col-2"></div>
+
+
+          <!-- collectionSelector -->
+          <div class="row mt-4">
+            <div class="col-2"></div>
+            <div class="col-8">
+              <collectionSelector />
+            </div>
+            <div class="col-2"></div>
+          </div>
+          <div class="row mt-4">
+            <div class="col-12">
+              <div
+                class="my-4 p-4 max-full bg-white border border-gray-100 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                <LicensesMain />
+              </div>
+            </div>
+          </div>
         </div>
-        <div id="plot"></div>
-      </div>  
-    </template>
-  </NuxtLayout>
+      </template>
+    </NuxtLayout>
   </div>
 </template>
 <script setup lang="ts">
-import { onMounted } from 'vue';
 import collectionSelector from "@/components/Observatory/CollectionSelector.vue"
-import Plotly from 'plotly.js-dist';
+import LicensesMain from "@/components/Observatory/trends/licenses/LicensesMain.vue"
+
 const layout = 'observatory'
 
 
-// prueba plotly
-onMounted(() => {
-  const data = [
-    {
-      x: ['giraffes', 'orangutans', 'monkeys'],
-      y: [20, 14, 23],
-      type: 'bar'
-    }
-  ];
-  Plotly.newPlot('plot', data);
-});
+
 
 </script>
