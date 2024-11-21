85ac29433c8418ac5d4bce319549b5d675b04732
Andrea <andrea.morales@bsc.es>
2024-11-21 11:54:57 +0100

    Fix extra space on iframe code

--- components/Observatory/PlotWOptions.vue
+++ components/Observatory/PlotWOptions.vue
@@ -1,49 +1,41 @@
 <template>
   <div class="mb-0 pb-0 ml-auto mr-0 pr-0 pt-2">
-    <UTooltip text="See iframe code" 
-      :ui="{background:'bg-black bg-opacity-50', color:'text-white', 
-      transition:{
+    <UTooltip text="See iframe code" :ui="{
+      background: 'bg-black bg-opacity-50', color: 'text-white',
+      transition: {
         enterActiveClass: 'transition ease-out duration-300',
         leaveActiveClass: 'transition ease-in duration-150',
-      }}"
-    >
-      <button 
-        @click="dialog = true" 
-        class="px-2 py-1 rounded-full hover:bg-gray-100 click-animation" 
-        aria-label="Copy icon"
-      >
-        <UIcon
-            name="i-oui-copy"
-            class="text-gray-500"
-        />
+      }
+    }">
+      <button @click="dialog = true" class="px-2 py-1 rounded-full hover:bg-gray-100 click-animation"
+        aria-label="Copy icon">
+        <UIcon name="i-oui-copy" class="text-gray-500" />
       </button>
     </UTooltip>
-    
-    <div v-if="dialog" class="fixed z-30 inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full flex items-center justify-center" @click.self="closeDialog">
+
+    <div v-if="dialog"
+      class="fixed z-30 inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full flex items-center justify-center"
+      @click.self="closeDialog">
       <div :class="['relative mx-auto p-4 border w-11/12 md:w-2/3 shadow-lg rounded-md bg-white', dialogAnimation]">
         <h5 class="mb-2">Plots Iframe Code</h5>
-        <p class="text-sm">Copy and paste the following code to embed the plots into your website. Change the width and height of the iframe as needed.</p>
+        <p class="text-sm">Copy and paste the following code to embed the plots into your website. Change the width and
+          height of the iframe as needed.</p>
         <div class="space-y-4">
           <div v-for="(item, index) in props.items" :key="index">
             <h6 class="text-sm text-primaryOeb-600">{{ item.title }}</h6>
             <div class="row">
               <div class="col-11">
                 <pre class="bg-gray-100 p-2 rounded">
-                  {{ copyIframeCode(item.url).trim() }}
+                  <div class="prueba">{{ copyIframeCode(item.url).trimStart() }}</div>
                 </pre>
               </div>
               <div class="col-1">
-              <UTooltip text="Copy to clipboard" :ui="{background:'bg-black bg-opacity-50', color:'text-white'}">
-                <button 
-                  @click="handleCopyClick(item.url)"
-                  class="px-2 py-1 rounded-full hover:bg-gray-100 click-animation"
-                >
-                <UIcon
-                    name="i-oui-copy"
-                    class="text-gray-500"
-                />
-                </button>
-              </UTooltip>
+                <UTooltip text="Copy to clipboard" :ui="{ background: 'bg-black bg-opacity-50', color: 'text-white' }">
+                  <button @click="handleCopyClick(item.url)"
+                    class="px-2 py-1 rounded-full hover:bg-gray-100 click-animation">
+                    <UIcon name="i-oui-copy" class="text-gray-500" />
+                  </button>
+                </UTooltip>
               </div>
             </div>
           </div>
@@ -51,7 +43,8 @@
       </div>
     </div>
 
-    <div v-if="copied" class="fixed z-40 bottom-10 left-1/2 rounded transform -translate-x-1/2 w-80 text-center p-2 bg-black bg-opacity-50 text-white flex justify-between items-center">
+    <div v-if="copied"
+      class="fixed z-40 bottom-10 left-1/2 rounded transform -translate-x-1/2 w-80 text-center p-2 bg-black bg-opacity-50 text-white flex justify-between items-center">
       <span>Copied to clipboard</span>
       <button @click="copied = false" class="ml-2 bg-transparent border-none text-red-600">Close</button>
     </div>
@@ -73,10 +66,9 @@ const dialogAnimation = ref('an1');
 const copyIframeCode = (url: string) => {
   const newUrl = `${url}/${props.currentCollection}/`;
   return `<iframe src="${newUrl}"
-    frameborder="0"
-    style="width:50%; height:500px;"
-    scrolling="no">
-  </iframe>`;
+  frameborder="0"
+  style="width:50%; height:500px;"
+  scrolling="no">\n</iframe>`;
 };
 
 // Show message Copied to clipboard
@@ -116,6 +108,10 @@ watch(dialog, (newValue) => {
 </script>
 
 <style scoped>
+
+.prueba{
+  padding-left: -10px;
+}
 .copy-icon {
   cursor: pointer;
 }
@@ -135,7 +131,7 @@ watch(dialog, (newValue) => {
 }
 
 .click-animation {
-  background-color: initial; 
+  background-color: initial;
 }
 
 .an1 {
@@ -143,13 +139,13 @@ watch(dialog, (newValue) => {
 }
 
 @keyframes animationup {
-    0% {
-        transform: scale(0.5);
-    }
+  0% {
+    transform: scale(0.5);
+  }
 
-    100% {
-        transform: scale(1);
-    }
+  100% {
+    transform: scale(1);
+  }
 }
 
 .an2 {
@@ -157,12 +153,12 @@ watch(dialog, (newValue) => {
 }
 
 @keyframes animationDown {
-    0% {
-        transform: scale(1);
-    }
+  0% {
+    transform: scale(1);
+  }
 
-    100% {
-        transform: scale(0);
-    }
+  100% {
+    transform: scale(0);
+  }
 }
 </style>
