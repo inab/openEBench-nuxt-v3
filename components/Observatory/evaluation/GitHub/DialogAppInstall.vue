fd08332a16b02ccfc4588f89ee5a7d7e44086892
avalle <ani.valle@bsc.es>
2024-11-25 16:42:03 +0100

    This componente will be implemented later

--- components/Observatory/evaluation/GitHub/DialogAppInstall.vue
+++ components/Observatory/evaluation/GitHub/DialogAppInstall.vue
@@ -1,13 +1,22 @@
 <template>
+  <!-- This componente will be implemented later -->
   <div class="flex justify-center">
     <div class="row">
       <div class="col-8">
-        <h1>Dialog</h1>
-
+        <!-- <h1>Dialog</h1> -->
       </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+// PROPS
+const props = defineProps<{
+  title: String,
+  text: String,
+  installationUrl: String,
+  initialDialogAppInstall: boolean,
+}>();
+
+
 </script>
