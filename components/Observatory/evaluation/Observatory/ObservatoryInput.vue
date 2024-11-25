04d2c8ef07f7b0965392a382ddd0555c915f4f91
avalle <ani.valle@bsc.es>
2024-11-25 16:40:00 +0100

    Adding step management buttons

--- components/Observatory/evaluation/Observatory/ObservatoryInput.vue
+++ components/Observatory/evaluation/Observatory/ObservatoryInput.vue
@@ -1,6 +1,32 @@
 <template>
-  <h1>Input</h1>
+  <h1>Observatory Input</h1>
+
+  <div class="row justify-center">
+    <div class="col-10 text-right">
+      <button type="button" class="btn btn-secondary mr-2" @click="goBack">Back</button>
+      <button
+        type="submit"
+        class="btn btn-primary"
+        @click="completeStep"
+      >
+        Continue
+      </button>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
+import { useStepperStore } from '@/stores/observatory/evaluation/index';
+
+const stepperStore = useStepperStore();
+
+// Methods
+const goBack = () => {
+  const currentStepIndex = 1; // Actualiza con el índice del paso actual
+  stepperStore.goBack(currentStepIndex);
+};
+const completeStep = () => {
+  const currentStepIndex = 1; // Actualiza con el índice del paso actual
+  stepperStore.completeStep(currentStepIndex);
+};
 </script>
