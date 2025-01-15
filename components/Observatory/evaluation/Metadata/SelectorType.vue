459ace7ff993135f29c6666098c57d396c27df51
avalle <ani.valle@bsc.es>
2025-01-15 17:45:41 +0100

    Create selector

---
+++ components/Observatory/evaluation/Metadata/SelectorType.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="row">
+    <USelectMenu
+      v-model="selectedType"
+      :options="typeItems"
+      class="mt-2 border-none"
+      bordered="false"
+      @update:model-value="changeValue"
+    >
+      <template #selected="{ option }">
+        {{ formatOption(option) }}
+      </template>
+      <template class="mt-2 border-none" #option="{ option }">
+        <div class="text-caption ">
+          {{ formatOption(option) }}
+        </div>
+      </template>
+    </USelectMenu>
+  </div>
+</template>
+
+<script setup>
+import { ref, watch } from 'vue';
+import { useMetadataStore } from '@/stores/observatory/evaluation/metadata';
+
+// Props
+const props = defineProps({
+  initialSelectedType: {
+    type: String,
+    required: false,
+  },
+  field: {
+    type: String,
+    required: true,
+  },
+});
+
+// Store
+const metadataStore = useMetadataStore();
+
+// Reactive variables
+const selectedType = ref(props.initialSelectedType);
+const typeItems = [
+  'cmd',
+  'web',
+  'api',
+  'lib',
+  'workflow',
+  'soap',
+  'db',
+  'suite',
+];
+
+// Methods
+const changeValue = () => {
+  const payload = {
+    field: props.field,
+    value: selectedType.value,
+  };
+  metadataStore.updateSelectorEntry(payload); // Usando el método definido en Pinia
+};
+
+// Watcher para sincronizar cambios en initialSelectedType
+watch(
+  () => props.initialSelectedType,
+  (newVal) => {
+    selectedType.value = newVal;
+  }
+);
+
+// Formatear la opción a mayúsculas
+const formatOption = (option) => {
+  return option ? String(option).toUpperCase() : '';
+};
+</script>
+
+<style scoped>
+/* Usamos una regla más específica para el dropdown del select */
+::v-deep .u-select-dropdown {
+  border: none !important; /* Elimina el borde */
+  box-shadow: none !important; /* Elimina la sombra */
+  padding: 0 !important; /* Elimina cualquier padding extra */
+}
+
+/* Asegúrate de que el select también no tenga borde si es necesario */
+::v-deep .u-select {
+  border: none !important;
+}
+
+.text-caption {
+  @apply text-sm text-gray-500;
+}
+</style>
