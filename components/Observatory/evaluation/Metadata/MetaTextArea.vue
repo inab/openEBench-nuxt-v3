617e0f05df6968e607bb404c541bac8984e837db
avalle <ani.valle@bsc.es>
2025-01-21 16:44:30 +0100

    add styles

--- components/Observatory/evaluation/Metadata/MetaTextArea.vue
+++ components/Observatory/evaluation/Metadata/MetaTextArea.vue
@@ -1,63 +1,59 @@
 <template>
-  <div class="container ms-3">
-    <div >
-      <div class="d-flex">
-        <HeaderAvatar
-          :field="field"
-          :field_name="title"
-          :disabled_tooltip="true"
+  <div class="container ms-3" :class="n_cols">
+    <div class="d-flex">
+      <HeaderAvatar
+        :field="field"
+        :field_name="title"
+        :disabled_tooltip="true"
+      />
+      <span class="text-body title">{{ title }}</span>
+      <UButton
+        color="gray"
+        variant="solid"
+        :ui="{ rounded: 'rounded-full' }"
+        class="p-1.5 mx-2"
+        @click="addEntry" 
+      >
+        <UIcon
+          name="i-heroicons-plus-circle-20-solid"
+          class="bg-gray-400"
         />
-        <span class="text-body title">{{ title }}</span>
+      </UButton>
+    </div>
+    <div class="mt-1 relative">
+      <UTextarea
+        v-for="(item, index) in description"
+        :key="item.id"
+        :value="item.term"
+        class=" d-flex border-1 rounded-md p-1"
+      >
+        <!-- clear description -->
+        <button
+          v-if="value"
+          class="absolute right-1"
+          @click="clearLink"
+        >
+          <UIcon
+            name="i-heroicons-x-circle-20-solid"
+            class="text-2xl"
+          />
+        </button>
+
+        <!-- Close Entry -->
         <UButton
           color="gray"
           variant="solid"
           :ui="{ rounded: 'rounded-full' }"
-          class="p-1.5 mx-2"
-          @click="addEntry" 
+          class="p-1.5 mx-3 absolute right-[-55px] top-0.5"
+          @click="closeEntry(index)" 
         >
           <UIcon
-            name="i-heroicons-plus-circle-20-solid"
+            name="i-heroicons-x-circle-20-solid"
             class="bg-gray-400"
           />
         </UButton>
-      </div>
-      <div class="mt-1 relative">
-        <UTextarea
-          v-for="(item, index) in description"
-          :key="item.id"
-			    :value="item.term"
-		      class="col-9 d-flex border-1 rounded-md p-1"
-        >
-          <!-- clear description -->
-          <button
-            v-if="value"
-            class="absolute right-1"
-            @click="clearLink"
-          >
-            <UIcon
-              name="i-heroicons-x-circle-20-solid"
-              class="text-2xl"
-            />
-          </button>
+      </UTextarea>
 
-          <!-- Close Entry -->
-          <UButton
-            color="gray"
-            variant="solid"
-            :ui="{ rounded: 'rounded-full' }"
-            class="p-1.5 mx-3 absolute right-[-60px] top-0"
-            @click="closeEntry(index)" 
-          >
-            <UIcon
-              name="i-heroicons-x-circle-20-solid"
-              class="bg-gray-400 text-lg"
-            />
-          </UButton>
-        </UTextarea>
-        <div class="col-3">
-          
-        </div>
-      </div>
     </div>
   </div>
 
