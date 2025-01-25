50712fa00ca7f0c996c0070c777ba0ff000fb425
Andrea <andrea.morales@bsc.es>
2025-01-25 01:03:05 +0100

    Fix observatory links in the footer

--- components/Header/HeaderMenu/subMenuEntriesObservatory.js
+++ components/Header/HeaderMenu/subMenuEntriesObservatory.js
@@ -1,12 +1,41 @@
 // Displayed in Header and Footer
 
 const tabsItems = [
-  { label: "Trends", slot: "trends", icon: "i-charm-chart-line", path: "observatory/", index: 0},
-  { label: "FAIRness Scoreboard", slot: "fairness", icon: "i-charm-disc", path: "observatory/", index: 1 },
-  { label: "FAIRsoft Evaluator", slot: "fairsoft", icon: "i-cil-check-circle", path: "observatory/", index: 2 },
-  { label: "Data", slot: "data", icon: "i-dashicons-database", path: "observatory/", index: 3 },
-  { label: "About", slot: "about", icon: "i-zondicons-information-solid", path: "observatory/", index: 4 },
+  {
+    label: "Trends",
+    slot: "trends",
+    icon: "i-charm-chart-line",
+    path: "observatory/",
+    index: 0,
+  },
+  {
+    label: "FAIRness Scoreboard",
+    slot: "fairness",
+    icon: "i-charm-disc",
+    path: "observatory/",
+    index: 1,
+  },
+  {
+    label: "FAIRsoft Evaluator",
+    slot: "fairsoft",
+    icon: "i-cil-check-circle",
+    path: "observatory/",
+    index: 2,
+  },
+  {
+    label: "Data",
+    slot: "data",
+    icon: "i-dashicons-database",
+    path: "observatory/",
+    index: 3,
+  },
+  {
+    label: "About",
+    slot: "about",
+    icon: "i-zondicons-information-solid",
+    path: "observatory/",
+    index: 4,
+  },
 ];
 
 export default tabsItems;
-
--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -11,28 +11,18 @@
         <!-- Col 2 -->
         <div class="col-md-6 col-12 p-4">
           <div class="row">
-            <div
-              v-for="(footer, index) in footers"
-              :key="index"
-              class="col-md-3 col-12 mb-2"
-            >
+            <div v-for="(footer, index) in footers" :key="index" class="col-md-3 col-12 mb-2">
               <!-- Button for mobile to toggle collapse -->
               <h5 class="d-md-none">
-                <button
-                  class="btn btn-link text-white d-flex align-items-center"
-                  type="button"
-                  :data-bs-toggle="'collapse'"
-                  :data-bs-target="'#collapse-' + index"
-                  aria-expanded="openIndex === index"
-                  :aria-controls="'collapse-' + index"
-                  @click="toggleCollapse(index)"
-                >
+                <button class="btn btn-link text-white d-flex align-items-center" type="button"
+                  :data-bs-toggle="'collapse'" :data-bs-target="'#collapse-' + index"
+                  aria-expanded="openIndex === index" :aria-controls="'collapse-' + index"
+                  @click="toggleCollapse(index)">
                   {{ footer.headline
-                  }}<UIcon
-                    name="i-heroicons-chevron-right-20-solid"
+                  }}
+                  <UIcon name="i-heroicons-chevron-right-20-solid"
                     class="transform transition-transform duration-200 ms-1"
-                    :class="[openIndex === index && 'rotate-90']"
-                  />
+                    :class="[openIndex === index && 'rotate-90']" />
                 </button>
               </h5>
 
@@ -40,35 +30,23 @@
               <h5 class="d-none d-md-block mb-2">{{ footer.headline }}</h5>
 
               <!-- Collapsible content -->
-              <div
-                :id="'collapse-' + index"
-                class="collapse"
-                :class="{ show: !isMobile }"
-              >
+              <div :id="'collapse-' + index" class="collapse" :class="{ show: !isMobile }">
                 <ul :class="{ 'list-unstyled': !isMobile }">
-                  <li v-for="(link, m) in footer.links" :key="m">
-                    <nuxt-link
-                      v-if="link.to"
-                      :to="link.to"
-                      class="text-white links-header"
-                    >
+                  <li v-for="(link, idx) in footer.links" :key="idx">
+                    <a v-if="footer.headline === 'Observatory'" href="#" class="text-white links-header"
+                      @click.prevent="goToObservatory(idx)">
+                      {{ link.label }}
+                    </a>
+                    <nuxt-link v-else-if="link.to" :to="link.to" class="text-white links-header">
                       {{ link.title }}
                     </nuxt-link>
-                    <a
-                      v-if="link.href"
-                      :href="link.href"
-                      target="_blank"
-                      class="text-white links-header"
-                    >
+                    <a v-else-if="link.href" :href="link.href" target="_blank" class="text-white links-header">
                       {{ link.title }}
-                      <font-awesome-icon
-                        :icon="['fas', 'arrow-up-right-from-square']"
-                        size="xs"
-                        class="mx-2"
-                      />
+                      <font-awesome-icon :icon="['fas', 'arrow-up-right-from-square']" size="xs" class="mx-2" />
                     </a>
                   </li>
                 </ul>
+
               </div>
             </div>
           </div>
@@ -77,50 +55,26 @@
         <!-- Col 3 -->
         <div class="col-md-3 col-12 p-4 col-funding">
           <div>
-            <a
-              href="mailto:openebench-support@bsc.es"
-              aria-label="Mail link for OpenEBench Support"
-              class="btn rounded-circle px-2 btn-f"
-            >
-              <font-awesome-icon
-                :icon="['far', 'envelope']"
-                size="lg"
-                style="color: #ffffff"
-              />
+            <a href="mailto:openebench-support@bsc.es" aria-label="Mail link for OpenEBench Support"
+              class="btn rounded-circle px-2 btn-f">
+              <font-awesome-icon :icon="['far', 'envelope']" size="lg" style="color: #ffffff" />
             </a>
-            <a
-              href="https://x.com/openebench"
-              target="_blank"
-              aria-label="Link to X Profile of OpenEBench"
-              class="btn rounded-circle p-2 btn-f"
-            >
-              <img
-                src="/assets/icons/twitter-x-64.png"
-                alt="X icon"
-                class="img-fluid"
-                style="max-width: 18px; max-height: 18px"
-              />
+            <a href="https://x.com/openebench" target="_blank" aria-label="Link to X Profile of OpenEBench"
+              class="btn rounded-circle p-2 btn-f">
+              <img src="/assets/icons/twitter-x-64.png" alt="X icon" class="img-fluid"
+                style="max-width: 18px; max-height: 18px" />
             </a>
           </div>
 
           <div class="row mt-2">
             <div class="col col-9">
               <p class="text-sm-left">
-                <small
-                  >OpenEBench receives funding from different EC funding
+                <small>OpenEBench receives funding from different EC funding
                   Research & Innovation programmes.<br />
-                  <a
-                    href="/collaboration"
-                    target="_blank"
-                    class="text-white links-header"
-                    aria-label="Learn more about collaboration"
-                  >
+                  <a href="/collaboration" target="_blank" class="text-white links-header"
+                    aria-label="Learn more about collaboration">
                     Learn more
-                    <font-awesome-icon
-                      :icon="['fas', 'arrow-up-right-from-square']"
-                      size="sm"
-                      class="mx-2"
-                    />
+                    <font-awesome-icon :icon="['fas', 'arrow-up-right-from-square']" size="sm" class="mx-2" />
                   </a>
                 </small>
               </p>
@@ -140,6 +94,7 @@ import logo from "~/assets/images/opeb_logo_white_minimal.png";
 import footerEntries from "~/components/footerEntries";
 import subMenuEntriesObservatory from "~/components/Header/HeaderMenu/subMenuEntriesObservatory.js";
 import subMenuEntriesAbout from "~/components/Header/HeaderMenu/subMenuEntriesAbout";
+import { activeTabIndex } from "@/components/Common/state.js";
 
 export default {
   data: () => ({
@@ -157,7 +112,7 @@ export default {
         ],
       },
     ],
-    isMobile: false, // Estado inicial
+    isMobile: false,
     openIndex: null,
   }),
 
@@ -166,24 +121,33 @@ export default {
       return new Date().getFullYear();
     },
   },
+
   mounted() {
     this.checkWindowSize();
     window.addEventListener("resize", this.checkWindowSize);
   },
+
   beforeUnmount() {
     window.removeEventListener("resize", this.checkWindowSize);
   },
+
   methods: {
     checkWindowSize() {
-      this.isMobile = window.innerWidth < 768; // Bootstrap's default mobile breakpoint
+      this.isMobile = window.innerWidth < 768;
     },
+
     toggleCollapse(index) {
-      // Alterna el índice de colapso entre el actual y null
       this.openIndex = this.openIndex === index ? null : index;
+    },
+
+    goToObservatory(index) {
+      activeTabIndex.value = index; // Update the global state for the active tab
+      this.$router.push("/observatory"); // Use `this.$router` to navigate
     },
   },
 };
 </script>
+
 
 <style scoped>
 .footer {
@@ -229,6 +193,7 @@ export default {
 
 /* Estilos para tablets */
 @media (min-width: 768px) and (max-width: 1024px) {
+
   /* Columna del listado */
   .col-md-6 {
     flex: 0 0 100%;
@@ -253,6 +218,7 @@ export default {
 
 /* Para pantallas más pequeñas */
 @media (max-width: 768px) {
+
   .col-md-6,
   .col-md-3 {
     flex: 0 0 100%;
@@ -263,9 +229,9 @@ export default {
 #svg-container {
   width: 100%;
   height: 50px;
-  background-image: url('~/assets/images/eu.svg'); 
+  background-image: url('~/assets/images/eu.svg');
   background-size: contain;
   background-repeat: no-repeat;
-  background-position: center; 
+  background-position: center;
 }
 </style>
