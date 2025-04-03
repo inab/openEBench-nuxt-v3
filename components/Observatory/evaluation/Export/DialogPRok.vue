40f645f2931c078ce452e8e2e662786a16f7e6b9
avalle <ani.valle@bsc.es>
2025-04-03 17:32:58 +0200

    Modals PR are done

--- components/Observatory/evaluation/Export/DialogPRok.vue
+++ components/Observatory/evaluation/Export/DialogPRok.vue
@@ -1,5 +1,65 @@
 <template>
+  <UModal v-model="dialogPRok" 
+    prevent-close 
+    :ui="{width:'w-96',overlay:{background:'bg-zinc-800/50'}}"
+  >
+    <div class="px-4 py-3">
+      <div class="row">
+        <div class="col-2">
+          <UIcon name="i-heroicons-check-circle" class="w-5 h-5" />
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-10">
+          <h6 class="text-center">Pull Request Created</h6>
+        </div>
+        <div class="col-2 flex justify-center">
+          <UButton 
+            color="gray"
+            variant="soft"
+            @click="openPRinGH()" 
+            class="mt-2"
+          >
+            See in repository
+          </UButton>
+        </div>
+      </div>
+
+      <!-- Actions -->
+      <div class="mt-2">
+        <UButton 
+          color="green"
+          variant="soft"
+          @click="close" 
+          class="mt-2"
+        >
+          Close
+        </UButton>
+      </div>
+    </div>
+  </UModal>
 
 </template>
 <script setup lang="ts">
+import { computed } from 'vue';
+import { useExport } from '@/stores/observatory/evaluation/export';
+const exportStore = useExport();
+
+const dialogPRok = computed (() => exportStore.getDialogPRok)
+const okURL = computed (() => exportStore.getOkURL)
+
+// Methods
+const close = () => {
+  exportStore.updateDialogPRok(false)
+}
+const openPRinGH = () => {
+  if (!okURL.value) {
+    console.error('okURL no está definido o es nulo');
+    return;
+  }
+
+  window.open(okURL.value, '_blank');
+};
+
+
 </script>
