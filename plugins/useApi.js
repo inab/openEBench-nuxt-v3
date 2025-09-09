b614ad2939375925ec67d67777bf1b77684f6cd7
avalle <ani.valle@bsc.es>
2025-09-09 12:49:53 +0200

    Fix the plugin configuration

--- plugins/useApi.js
+++ plugins/useApi.js
@@ -1,8 +1,14 @@
 export default defineNuxtPlugin(() => {
+  const runtimeConfig = useRuntimeConfig();
+  
   const api = $fetch.create({
     headers: {
-      Accept: "text/plain, */*",
+      common: {
+        Accept: "text/plain, */*",
+      },
     },
+    baseURL: runtimeConfig.public.MONITORING.baseURL,
+    method: "POST",
   });
 
   return {
