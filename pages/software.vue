eba069c7ddf35ae81fef7e1ae765797dd79bcb4a
avalle <ani.valle@bsc.es>
2026-07-21 16:15:58 +0200

    Create software page

---
+++ pages/software.vue
@@ -0,0 +1,11 @@
+<template>
+
+  <div>
+    <!-- Main Content -->
+    <div class="content p-4">
+      <div class="row my-4">
+        <h3 class="fw-bold mb-3">The Software Observatory</h3>
+      </div>
+    </div>
+  </div>
+</template>
