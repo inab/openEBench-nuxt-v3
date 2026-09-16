a09c7cf397df4151276205affb8f8b7c5c8189dd
avalle <ani.valle@bsc.es>
2026-09-16 16:12:22 +0200

    Create Chip Type

---
+++ components/Tools/Search/Card/ChipType.vue
@@ -0,0 +1,25 @@
+<template>
+  <UBadge class="chip-type px-2 mr-2" variant="outline" size="md">{{ text }}</UBadge>
+</template>
+  <script setup lang="ts">
+  import { computed } from 'vue';
+  import { getSoftwareTypeDescription } from '@/static/dictionaries/softwareTypes';
+  
+  const props = defineProps<{
+    type: string;
+  }>();
+  
+  const text = computed(() => getSoftwareTypeDescription(props.type));
+  </script>
+  
+  <style scoped>
+  .chip-type {
+    color: #0b579f;
+    height: 30px;
+  }
+  
+  .chip-type--small {
+    font-size: 0.75rem;
+    padding: 0.25em 0.6em;
+  }
+  </style>
